0383983e11696c3a6bd5c2ddef2b3090ad230aa6
AlexaGTorres <90784633+AlexaGTorres@users.noreply.github.com>
2021-10-19 18:33:27 -0700

    Update StudentDriver.java

--- src/StudentDriver.java
+++ src/StudentDriver.java
@@ -8,17 +8,28 @@ public class StudentDriver
 
         //toString
         System.out.println(turing.toString());
+        System.out.println(" ");
 
-        //Setters and Getters
         System.out.println (nightingale.toString() + "\n");
+
+        System.out.println (hopper.toString() + "\n");
         nightingale.setID(95266);
         nightingale.setGPA(3.92);
-        System.out.println("Florence ID: " + nightingale.getID() + ". Florence GPA: " + nightingale.getGpa());
+        System.out.println("Florence ID: " + nightingale.getID() + " Florence GPA: " + nightingale.getGpa());
+        System.out.println (" ");
 
-        System.out.println (hopper.getFirstName() + hopper.getCSStudent());
+        System.out.println (hopper.getFirstName() + " " + hopper.getCSStudent());
+        System.out.println (" ");
 
         //CreateID
-        
+        System.out.println ("Alan ID: " + turing.CreateID());
+        System.out.println ("Florance ID: " + nightingale.CreateID());
+        System.out.println ("Grace ID: " + hopper.CreateID());
+        System.out.println (" ");
+
         //CSGPA
+        System.out.println ("Alan CS Gpa: " + turing.csGPA());
+        System.out.println ("Florance CS Gpa: " + nightingale.csGPA());
+        System.out.println ("Grace CS Gpa: " + hopper.csGPA());
     }//ENd main class
 }//End StudentDriver
